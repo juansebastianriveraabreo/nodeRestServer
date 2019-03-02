d1be71fb682ba14f034302c8df1aff20485397e4
JuanSe <juan.sebastian.rivera.abreo@gmail.com>
2019-03-02 09:00:07 -0500

    Mi primer RESTServer

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/usuario', function (req, res) {
+  	res.json('get Usuarios');
+});
+
+app.post('/usuario', function (req, res) {
+	let body = req.body;
+	if(body === undefined){
+		res.status(400).json({
+			ok : false,
+			mensaje : 'El nombre es necesario'
+		});
+	}else{
+		res.json({body});	
+	}
+});
+
+app.put('/usuario/:id', function (req, res) {
+	let id = req.params.id;
+  	res.json({id});
+});
+
+app.delete('/usuario', function (req, res) {
+  	res.json('delete Usuarios');
+});
+ 
+app.listen(process.env.PORT, () => {
+	console.log('Escuchando el puerto',process.env.PORT);
+});
